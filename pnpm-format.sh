dd0e2b469c726e6d0e78728670d7f06445f4cf64
thilllon <thilllon970@gmail.com>
2023-01-02 11:56:04 +0900

    feat: add commitlint, codeformatting

---
+++ pnpm-format.sh
@@ -0,0 +1,31 @@
+pnpm add -D husky lint-staged prettier commitlint @commitlint/config-conventional @commitlint/cli commitizen git-cz
+
+# husky https://typicode.github.io/husky/#/
+pnpm dlx husky-init
+pnpm install
+npx npm-add-script --force --key "prepare" --value "husky install && chmod +x .husky/*"
+
+# lint-staged https://github.com/okonet/lint-staged#configuration
+echo "pnpm lint-staged" >>.husky/pre-commit
+echo '{
+  "./**/src/**/*": ["prettier -w -l", "eslint --fix"]
+}' >.lintstagedrc.json
+
+# commitizen https://github.com/commitizen/cz-cli
+echo '{
+  "path": "cz-conventional-changelog"
+}' >.czrc
+
+# commitlint https://github.com/conventional-changelog/commitlint
+npx husky add .husky/commit-msg 'pnpm commitlint --edit ${1}'
+echo "module.exports = {extends: ['@commitlint/config-conventional']}" >commitlint.config.js
+
+# prettier
+echo '{
+  "printWidth": 100,
+  "singleQuote": true
+}' >.prettierrc.json
+echo 'dist' >>.prettierignore
+echo 'pnpm-lock.yaml' >>.prettierignore
+npx npm-add-script --force --key "format" --value "prettier --write --list-different ."
+pnpm format

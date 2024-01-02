6e0dac5d2bcef5e2d7538f0308a86dccb1425a09
thilllon <thilllon970@gmail.com>
2024-01-02 13:18:45 +0900

    feat: delete deployment

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,15 @@
 declare module NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test';
+
     GITARIST_OWNER: string;
     GITARIST_REPO: string;
     GITARIST_TOKEN: string;
-    GITLAB_TOKEN: string;
+
     GITLAB_PROJECT_ID: string;
+    GITLAB_TOKEN: string;
+
+    VERCEL_PROJECT_ID: string;
+    VERCEL_TOKEN: string;
   }
 }
